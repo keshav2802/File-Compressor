2fe055f3720048a7c6bc049a51b6f8829fa7f13d
keshavism28 <keshavism28@gmail.com>
2020-09-28 12:21:31 +0530

    created the public folder dynamically.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const fs = require('fs');
+const multer = require('multer');
+
 const app = express();
+
+let dir = 'public';
+let subdir = 'public/uploads';
+
+if(!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+  fs.mkdirSync(subdir);
+}
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
 app.post('/compressfiles', (req, res) => {
-  
+
 })
 
 
