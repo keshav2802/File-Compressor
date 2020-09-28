24c3d30e9e98370fa77b5fcf6beb69b64cdcb767
keshavism28 <keshavism28@gmail.com>
2020-09-28 12:50:00 +0530

    upload the files

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const fs = require('fs');
 const multer = require('multer');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static('public'));
+
+// Create the public folder dynamically so that files can be uploaded to that folder.
 
 let dir = 'public';
 let subdir = 'public/uploads';
@@ -12,12 +17,34 @@ if(!fs.existsSync(dir)) {
   fs.mkdirSync(subdir);
 }
 
+// After creating the directories we will use the diskStorage method of multer library to store the files. We are telling multer where to store these files.
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+
+let maxSize = 10 * 1024 * 1024; // max 10MB of uploads
+var config = multer({ storage: storage,limits:{fileSize:maxSize}});
+
+// Convert the uploaded files into an array. Max files that can be uploaded is 100.
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/compressfiles', (req, res) => {
-
+app.post('/compressfiles', config.array('files', 100), (req, res) => {
+  if(req.files) {
+    req.files.forEach(file => {
+      console.log(file.path);
+    })
+  }
 })
 
 
