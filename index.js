e1ead2c8d9d168a3734bcd2f3a70e3dbed0108ec
keshavism28 <keshavism28@gmail.com>
2020-09-28 12:09:42 +0530

    created the form for selecting and uploading files

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/compressfiles', (req, res) => {
+  
 })
 
 
